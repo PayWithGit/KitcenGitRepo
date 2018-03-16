6aa593144a0cd817b451374e93cc1918259d7e62
p2kumar <p2kumar@N-20HMPC0RJKPX.nsn-intra.net>
2018-03-16 17:09:16 +0530

    initial commite

---
+++ KitchenRecipesProj/src/com/kitchen/recipes/Roti.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.kitchen.recipes;
+
+/**
+ * @author p2kumar
+ *
+ */
+public class Roti {
+	private Long width;
+
+	public Long getWidth() {
+		return width;
+	}
+
+	public void setWidth(Long width) {
+		this.width = width;
+	}
+	
+}
