fdc205244cc80d29b9a3b453fe64627347ad6450
Kumar <pankaj.13.kumar.ext@nokia.com>
2018-03-17 11:02:38 +0530

    1st commite sts Chapati.java

---
+++ KitchenRecipesProj/src/com/kitchen/recipes/Chapati.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.kitchen.recipes;
+
+/**
+ * @author p2kumar
+ *
+ */
+public class Chapati {
+	//1st making comment from sts Chapati.java  
+	private Long width;
+
+	public Long getWidth() {
+		return width;
+	}
+
+	public void setWidth(Long width) {
+		this.width = width;
+	}
+	
+
+}
